5dddb8e4e82d9e1bc256938931d5f143c1909121
jcamilo97 <jcamilo97@gmail.com>
2018-10-01 17:06:22 -0500

    init repo

---
+++ src/main/java/com/ubosque/sgdaubosque/SgdaUbosqueApplication.java
@@ -0,0 +1,12 @@
+package com.ubosque.sgdaubosque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgdaUbosqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgdaUbosqueApplication.class, args);
+	}
+}
